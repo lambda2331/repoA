e67ca114c98366b4d76a39004ea258a5ad8f4fa4
noamski <noamdob@gmail.com>
2024-08-07 11:42:32 +0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import request = require('request');
 
 import { User } from './config_types';
 
+const SNYK_TOKEN=09e1705e-9b8c-4fbc****************** snyk test --all-projects --detection-depth=15 "CLONEDIR" --json-file-output="OUTPUTPATH${newTool.fileNameOutput}" > /dev/null'`;
 export interface Authenticator {
     isAuthProvider(user: User): boolean;
     applyAuthentication(user: User, opts: request.Options | https.RequestOptions): Promise<void>;
